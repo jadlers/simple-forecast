7ba20ff2e7dca24818c2d533bbb30a37342931ad
Jacob Adlers <jacob.adlers@gmail.com>
2018-03-10 17:04:00 +0100

    Add toggle to show five day forecast for city

--- src/city.js
+++ src/city.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
+import { fiveDayForecast } from './api-requests';
+
+import Day from './day.js';
 
 class City extends Component {
+  state = {
+    expanded: false,
+    forecast: {},
+  };
+
+  async toggleDetail(woeid) {
+    let { expanded, forecast } = this.state;
+    if (!expanded) {
+      forecast = await fiveDayForecast(woeid);
+    }
+    this.setState({ forecast, expanded: !expanded });
+  }
+
   render() {
     const { data } = this.props;
+    const { expanded, forecast } = this.state;
 
     return (
       <div>
-        <h1>{data.title}</h1>
-        <h2>{data.location_type}</h2>
-        {data.woeid}
+        <div
+          style={{ border: '1px solid black' }}
+          onClick={() => this.toggleDetail(data.woeid)}
+        >
+          <h1>{data.title}</h1>
+          <h2>{data.location_type}</h2>
+          {data.woeid}
+        </div>
+        <div>
+          {expanded &&
+            forecast.consolidated_weather.map(v => <Day key={v.id} data={v} />)}
+        </div>
       </div>
     );
   }
