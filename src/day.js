5d0161eeacc41eb6f3afe560c7f9b40c01babfed
Jacob Adlers <jacob.adlers@gmail.com>
2018-03-16 18:42:56 +0100

    Add styling to Day component
    Mobile first design

--- src/day.js
+++ src/day.js
@@ -1,16 +1,34 @@
 import React from 'react';
 
+import './day.css';
 import { weatherStateIcon } from './api-requests';
 
 const Day = ({ data }) => {
   const date = new Date(data.Date);
   const weatherIcon = weatherStateIcon(data.Day.Icon);
+  const { Maximum, Minimum } = data.Temperature;
+
   return (
-    <div>
-      <img src={weatherIcon} alt={data.Day.IconPhrase} />
-      <p>{date.toDateString()}</p>
-      <p>Max: {data.Temperature.Maximum.Value}</p>
-      <p>Min: {data.Temperature.Minimum.Value}</p>
+    <div className="day">
+      <p className="date">{date.toDateString()}</p>
+      <div>
+        <img src={weatherIcon} alt={data.Day.IconPhrase} />
+
+        <div>
+          <p>
+            <span className="fas fa-long-arrow-alt-up" />
+            {Maximum.Value}&deg;{Maximum.Unit}
+          </p>
+
+          <p>
+            <span
+              className="fas fa-long-arrow-alt-down"
+              style={{ color: '#555' }}
+            />
+            {Minimum.Value}&deg;{Minimum.Unit}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
